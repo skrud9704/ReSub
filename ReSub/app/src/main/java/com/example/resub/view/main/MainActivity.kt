b493bf4d55333d2a985e50b2f3a6271d2ba9a73e
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2020-11-18 14:46:43 +0900

    MainActivity에 BottomNavigation 추가
    하단 메뉴추가, 구독 추가버튼 추가.

--- ReSub/app/src/main/java/com/example/resub/view/main/MainActivity.kt
+++ ReSub/app/src/main/java/com/example/resub/view/main/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.resub.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.resub.CustomBottomNavigation
 import com.example.resub.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation(){
+        custom_bottom_nav.inflateMenu(R.menu.bottom_navigation_menu)
     }
 }
