81dbba86f9322d65d20b5976bb2a0ca53ee48d3f
skrud9704 <43378081+skrud9704@users.noreply.github.com>
2020-12-08 18:20:29 +0900

    SingleRowCalendar 커스텀 완료
    ReSub에 맞게 커스텀 완료.
    좌우 화살표 클릭시 월바뀌는 것 차후 추가.

--- ReSub/app/src/main/java/com/example/resub/view/register/RegisterActivity.kt
+++ ReSub/app/src/main/java/com/example/resub/view/register/RegisterActivity.kt
@@ -1,17 +1,183 @@
 package com.example.resub.view.register
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.resub.R
+import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
+import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
+import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
+import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
+import com.michalsvec.singlerowcalendar.utils.DateUtils
+import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.calendar_unselected_item.view.*
+import java.util.*
+
 
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val calendar = Calendar.getInstance()
+    private var currentMonth = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        setCalender()
+    }
 
+
+    @SuppressLint("SetTextI18n")
+    private fun setCalender(){
+
+        // set current date to calendar and current month to currentMonth variable
+        calendar.time = Date()
+        currentMonth = calendar[Calendar.MONTH]
+
+        tvYear.text = "${DateUtils.getYear(calendar.time)}년 "
+        tvMonth.text = "${DateUtils.getMonthNumber(calendar.time)}월 "
+
+        // enable white status bar with black icons
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        window.statusBarColor = Color.WHITE
+
+        // calendar view manager is responsible for our displaying logic
+        val myCalendarViewManager = object :
+            CalendarViewManager {
+            override fun setCalendarViewResourceId(
+                position: Int,
+                date: Date,
+                isSelected: Boolean
+            ): Int {
+                // set date to calendar according to position where we are
+                val cal = Calendar.getInstance()
+                cal.time = date
+                // if item is selected we return this layout items
+                // in this example. monday, wednesday and friday will have special item views and other days
+                // will be using basic item view
+                return if (isSelected)
+                    when (cal[Calendar.DAY_OF_WEEK]) {
+                        /*Calendar.MONDAY -> R.layout.first_special_selected_calendar_item
+                        Calendar.WEDNESDAY -> R.layout.second_special_selected_calendar_item
+                        Calendar.FRIDAY -> R.layout.third_special_selected_calendar_item*/
+                        else -> R.layout.calendar_selected_item
+                    }
+                else
+                // here we return items which are not selected
+                    when (cal[Calendar.DAY_OF_WEEK]) {
+                        /*Calendar.MONDAY -> R.layout.first_special_calendar_item
+                        Calendar.WEDNESDAY -> R.layout.second_special_calendar_item
+                        Calendar.FRIDAY -> R.layout.third_special_calendar_item*/
+                        else -> R.layout.calendar_unselected_item
+                    }
+
+                // NOTE: if we don't want to do it this way, we can simply change color of background
+                // in bindDataToCalendarView method
+            }
+
+            override fun bindDataToCalendarView(
+                holder: SingleRowCalendarAdapter.CalendarViewHolder,
+                date: Date,
+                position: Int,
+                isSelected: Boolean
+            ) {
+                // using this method we can bind data to calendar view
+                // good practice is if all views in layout have same IDs in all item views
+                holder.itemView.tv_date_calendar_item.text = DateUtils.getDayNumber(date)
+                holder.itemView.tv_day_calendar_item.text = DateUtils.getDay3LettersName(date)
+
+            }
+        }
+
+        // using calendar changes observer we can track changes in calendar
+        val myCalendarChangesObserver = object :
+            CalendarChangesObserver {
+            // you can override more methods, in this example we need only this one
+            override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
+                tvYear.text = "${DateUtils.getYear(date)}년 "
+                tvMonth.text = "${DateUtils.getMonthNumber(date)}월 "
+                super.whenSelectionChanged(isSelected, position, date)
+            }
+
+
+        }
+
+        // selection manager is responsible for managing selection
+        val mySelectionManager = object :
+            CalendarSelectionManager {
+            override fun canBeItemSelected(position: Int, date: Date): Boolean {
+                // set date to calendar according to position
+                val cal = Calendar.getInstance()
+                cal.time = date
+                // in this example sunday and saturday can't be selected, others can
+                return when (cal[Calendar.DAY_OF_WEEK]) {
+                    /*Calendar.SATURDAY -> false
+                    Calendar.SUNDAY -> false*/
+                    else -> true
+                }
+            }
+        }
+
+        // here we init our calendar, also you can set more properties if you haven't specified in XML layout
+        val singleRowCalendar = main_single_row_calendar.apply {
+            this.calendarViewManager = myCalendarViewManager
+            this.calendarChangesObserver = myCalendarChangesObserver
+            this.calendarSelectionManager = mySelectionManager
+            setDates(getFutureDatesOfCurrentMonth())
+            init()
+        }
+
+        btnRight.setOnClickListener {
+            singleRowCalendar.setDates(getDatesOfNextMonth())
+        }
+
+        btnLeft.setOnClickListener {
+            singleRowCalendar.setDates(getDatesOfPreviousMonth())
+        }
+    }
+
+    private fun getDatesOfNextMonth(): List<Date> {
+        currentMonth++ // + because we want next month
+        if (currentMonth == 12) {
+            // we will switch to january of next year, when we reach last month of year
+            calendar.set(Calendar.YEAR, calendar[Calendar.YEAR] + 1)
+            currentMonth = 0 // 0 == january
+        }
+        return getDates(mutableListOf())
+    }
+
+    private fun getDatesOfPreviousMonth(): List<Date> {
+        currentMonth-- // - because we want previous month
+        if (currentMonth == -1) {
+            // we will switch to december of previous year, when we reach first month of year
+            calendar.set(Calendar.YEAR, calendar[Calendar.YEAR] - 1)
+            currentMonth = 11 // 11 == december
+        }
+        return getDates(mutableListOf())
+    }
+
+    private fun getFutureDatesOfCurrentMonth(): List<Date> {
+        // get all next dates of current month
+        currentMonth = calendar[Calendar.MONTH]
+        return getDates(mutableListOf())
+    }
+
+
+    private fun getDates(list: MutableList<Date>): List<Date> {
+        // load dates of whole month
+        calendar.set(Calendar.MONTH, currentMonth)
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        list.add(calendar.time)
+        while (currentMonth == calendar[Calendar.MONTH]) {
+            calendar.add(Calendar.DATE, +1)
+            if (calendar[Calendar.MONTH] == currentMonth)
+                list.add(calendar.time)
+        }
+        calendar.add(Calendar.DATE, -1)
+        return list
     }
 
 }
